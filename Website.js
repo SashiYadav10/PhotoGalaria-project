4822a4d6604b64fab69192a1f63f7a9e5041d891
Sashi <sashi.bhusan.yadav@stud.th-luebeck.de>
2023-11-18 22:29:07 +0100

    created page to add friend with day and night mode

---
+++ Website.js
@@ -0,0 +1,43 @@
+var light = document.querySelector("#mode")
+var btn = document.querySelector("#btn")
+var statuss = document.querySelector("h2")
+var page = document.querySelector("html")
+var fr = document.querySelector("#frame")
+
+var lightCnt = 0
+light.addEventListener("click", function() {
+    if(lightCnt == 0) {
+        light.innerHTML = "Day Mode"
+        light.style.backgroundColor = "Black"
+        light.style.color = "White"
+        page.style.backgroundColor = "Black"
+        btn.style.color = "White"
+        btn.style.backgroundColor = "Black"
+        fr.style.backgroundColor = "Black"
+        lightCnt = 1
+    } else {
+        light.innerHTML = "Night Mode"
+        light.style.backgroundColor = "White"
+        light.style.color = "Black"
+        page.style.backgroundColor = "White"
+        btn.style.color = "Black"
+        btn.style.backgroundColor = "white"
+        fr.style.backgroundColor = "White"
+        lightCnt = 0
+    }
+})
+var stat = 0
+btn.addEventListener("click", function() {
+    if(stat == 0) {
+        btn.innerHTML = "Remove"
+        statuss.innerHTML = "Friends"
+        statuss.style.color = "Green"
+        stat = 1
+    } else {
+        btn.innerHTML = "Add Friend"
+        statuss.innerHTML = "Stranger"
+        statuss.style.color = "Red"
+        stat = 0
+    }  
+})
+
